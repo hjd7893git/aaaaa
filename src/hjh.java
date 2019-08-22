041662a9d766d693cbe4dd96c0e6c95958375f26
huangjunhua <740920822@qq.com>
2019-08-22 10:49:18 +0800

    hjh第4次提交

--- src/hjh.java
+++ src/hjh.java
@@ -2,7 +2,6 @@ import java.sql.SQLOutput;
 
 public class hjh {
     public static void main(String[] args) {
-        System.out.println("new file3333");
+        System.out.println("new file4444");
     }
-    123
 }

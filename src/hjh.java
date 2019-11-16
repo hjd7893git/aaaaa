adcb069e6549ec5d57fd46d326472af0b9818b60
hjd7893git <1622954512@qq.com>
2019-11-16 17:48:24 +0800

    收到v发

--- src/hjh.java
+++ src/hjh.java
@@ -1,7 +1,3 @@
 import java.sql.SQLOutput;
 
 public class hjh {
-    public static void main(String[] args) {
-        System.out.println("new file111111111111111144444444444444444444444444444444444423132132123");
-    }가가가가가가
-}

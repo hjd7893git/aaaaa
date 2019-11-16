1025bc7a1ca8462ebc115063525161942e7d09a3
hjd7893git <1622954512@qq.com>
2019-11-16 17:43:15 +0800

    aaaaa

--- src/hjh.java
+++ src/hjh.java
@@ -2,10 +2,6 @@ import java.sql.SQLOutput;
 
 public class hjh {
     public static void main(String[] args) {
-        System.out.println("new file555");
-        1111
-        2222
-        ±¾µØÐÞ¸Ä9
-                nei fen zhi
+        System.out.println("new file111111111111111144444444444444444444444444444444444423132132123");
     }
 }
